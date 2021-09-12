d1c3c227bcbe3b379c8eda960c1f87e77fe1dbb9
s211427@students.aie.edu.au <s211427@students.aie.edu.au>
2021-09-12 14:19:01 +1000

    half-add double linked list

--- linkedlist/include/list.hpp
+++ linkedlist/include/list.hpp
@@ -1,11 +1,109 @@
 #pragma once
 
+/* Represents a single list node */
 template <typename T>
 struct list_node {
-	
+	T value;
+	list_node<T>* next = nullptr;
+	list_node<T>* prev = nullptr;
+
+	list_node(T v) {
+		value = v;
+	}
 };
 
-/* Implements a double linked list */
+/* Implements a double linked list
+ *
+ * Almost the entirety of this structure is
+ * implemented in the header, since that is
+ * required for templates. */
+template <typename T>
 struct list {
-	
+	list_node<T>* head = nullptr;
+	list_node<T>* tail = nullptr;
+
+	list_node<T>* insert(list_node<T>* node, const T& v) {
+		list_node<T>* nn = new list_node<T>(v);
+
+		nn->next = node;
+
+		if (node) {
+			nn->prev = node->prev;
+
+			if (node->prev) {
+				node->prev->next = nn;
+			}
+			node->prev = nn;
+		}
+
+		if (head == node) {
+			head = nn;
+		}
+
+		if (!tail) {
+			tail = nn;
+		}
+
+		return nn;
+	}
+
+	void remove(list_node<T>* node) {
+		if (!head || !node) { return; }
+
+		if (head == node) {
+			head = node->next;
+		}
+
+		if (node->next) {
+			node->next->prev = node->prev;
+		}
+
+		if (node->prev) {
+			node->prev->next = node->next;
+		}
+
+		delete node;
+	}
+
+	list_node<T>* push_front(const T& v) {
+		return insert(head, v);
+	}
+
+	list_node<T>* push_back(const T& v) {
+		return insert(tail, v);
+	}
+
+	int count() {
+		list_node<T>* cur = head;
+
+		int r = 0;
+
+		while (cur) {
+			cur = cur->next;
+
+			r++;
+		}
+
+		return r;
+	}
+
+	list_node<T>* first() {
+		return head;
+	}
+
+	list_node<T>* last() {
+		return tail;
+	}
+
+	bool empty() { return !head; }
+
+	~list() {
+		list_node<T>* cur = head;
+
+		while (cur) {
+			list_node<T>* temp = cur;
+			cur = cur->next;
+			delete temp;
+		}
+	}
 };
--- linkedlist/src/main.cpp
+++ linkedlist/src/main.cpp
@@ -2,7 +2,7 @@
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <imgui_impl_opengl2.h>
 
 /* The reason why I chose to use Dear ImGui over RayGUI is because
  * Dear ImGui is simply a better GUI. More difficult to set up, and
@@ -16,7 +16,12 @@
  * I chose GLFW as the windowing and input backend and OpenGL as the
  * rendering backend because those are what I'm familiar with, and
  * they are quick and easy to setup in comparison to the other
- * Dear ImGui backends such as the Vulkan or DirectX backends. */
+ * Dear ImGui backends such as the Vulkan or DirectX backends.
+ *
+ * I'm using the OpenGL 2 backend instead of the modern one
+ * because my Windows virtual machine doesn't support OpenGL 3.x*/
+
+#include "list.hpp"
 
 static void framebuffer_size_func(GLFWwindow* window, int x, int y) {
 	glViewport(0, 0, x, y);
@@ -24,11 +29,6 @@ static void framebuffer_size_func(GLFWwindow* window, int x, int y) {
 
 int main() {
 	glfwInit();
-
-	/* The minimum OpenGL version that the modern Dear ImGui backend
-	 * supports. */
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
 	GLFWwindow* window = glfwCreateWindow(1024, 768, "Linked List demo", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
@@ -45,25 +45,54 @@ int main() {
 	ImGui::StyleColorsLight();
 
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 130");
+	ImGui_ImplOpenGL2_Init();
+
+	list<int> some_list;
+	int create_value = 0;
+
+	list_node<int>* selected_node = nullptr;
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
-		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplOpenGL2_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		ImGui::ShowDemoWindow();
+		if (ImGui::Begin("Linked List Demo")) {
+			ImGui::Text("List count: %d", some_list.count());
+
+			ImGui::DragInt("###VINPUT", &create_value); ImGui::SameLine();
+
+			if (ImGui::Button("Push Front")) {
+				some_list.push_front(create_value);
+			} ImGui::SameLine();
+
+			if (ImGui::Button("Push Back")) {
+				some_list.push_back(create_value);
+			}
+
+			list_node<int>* cur = some_list.head;
+			while (cur) {
+				ImGui::Text("List item: %d", cur->value);
+				ImGui::SameLine();
+				if (ImGui::Button("Delete Item")) {
+					some_list.remove(cur);
+				}
+
+				cur = cur->next;
+			}
+		}
+		ImGui::End();
 
 		ImGui::Render();
 		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 
 		glfwSwapBuffers(window);
 	}
 
-	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplOpenGL2_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 
 	ImGui::DestroyContext();
